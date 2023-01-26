d5e14fa044102c19845da552befe2a423e29d0c9
VadMack <makarovvadim01@icloud.com>
2023-01-26 19:56:00 +0300

    #27: MongoDB cluster

---
+++ server/scripts/rs-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Sleeping for 5 seconds"
+sleep 5
+
+echo mongo_setup.sh time now: `date +"%T" `
+mongosh --host mongo:27017 <<EOF
+  var cfg = {
+    "_id": "rs0",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo:27017",
+        "priority": 2
+      }
+    ]
+  };
+  rs.initiate(cfg);
+EOF
+
+echo "User initialization is starting"
+mongosh --host mongo:27017 < /scripts/init-user.js
+echo "User initialized successfully"
